e058c852433bb4c01933a83e1220c3b87a80944b
BenV-V <70845425+BenV-V@users.noreply.github.com>
2021-01-23 17:49:31 +0100

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,35 +1,35 @@
-// récupération de l'API
-fetch('http://localhost:3000/api/cameras')
+class Products{
+  constructor(element){
+      this.$el = element;
 
-    .then(response => {return response.json()})
+      this.display();
+  }
+    
+  display(){    
 
-    .then((response) => {for(let i = 0; i < response.length; i++) {
-        class Response {
-            constructor(image, name, description, price, _id){
-                this.image = image;
-                this.name = name;
-                this.description= description;
-                this.price = price;
-                this._id = _id;
-            }
-        }
-        let newResponse = new Response(
-            response[i].imageUrl,
-            response[i].name,
-            response[i].description,
-            response[i].price,
-            response[i]._id,        
-        )
-        // intégration dans le fichier index.html dans la div #cameras pour chaque réponse
-        document.querySelector('#cameras').innerHTML +=  
+      fetch('http://localhost:3000/api/cameras')
+          .then(response => {return response.json()})
+          .then(item =>  {
+              item.forEach(product => {
+
+              let html = 
                   `<div class="card">    
-                    <img src="${newResponse.image}" class="card-img-top" alt="Images cameras">
-                    <div class="card-body">
-                      <h3 class="card-title">${newResponse.name}</h3>
-                      <p class="card-text">${newResponse.description}</p>
-                      <p class="row">Prix : ${(newResponse.price/100)} €</p>
-                      <a href="./produit.html?${newResponse._id}" class="btn btn-primary stretched-link">Voir l'article</a>
-                    </div>
-                  </div>`
-    }})
+                      <img src="${product.imageUrl}" class="card-img-top" alt="Images cameras">
+                      <div class="card-body">
+                      <h3 class="card-title">${product.name}</h3>
+                      <p class="card-text">${product.description}</p>
+                      <p class="row">Prix : ${(product.price/100)} €</p>
+                      <a href="./produit.html?${product._id}" class="btn btn-primary stretched-link">Voir l'article</a>
+                      </div>
+                  </div>`;
+      
+          this.$el.insertAdjacentHTML('beforeend', html);
+          });
+      })
+  }
+}
+
+const products = new Products(document.getElementById('cameras'));
+
+  
 
